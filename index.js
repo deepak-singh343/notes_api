c71b097505de5e0427b282e0ba39bbd40b0fb6df
Deepak Singh <deepak.negi343@gmail.com>
2020-03-29 11:11:12 +0530

    created notes_api

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express=require('express');
 const app=express();
-
 const port=8000;
 const db=require('./config/mongoose');
 const bodyParser= require('body-parser')
--- routes/getNote.js
+++ routes/getNote.js
@@ -1,5 +1,7 @@
 const express=require('express');
 const router=express.Router();
+// const passport=require('passport');
 const getNotecontroller=require('../controllers/getNote_controller');
 router.get('/',getNotecontroller.getNote);
+// router.get('/:id',passport.authenticate('jwt',{session:false}),getNotecontroller.getNote);
 module.exports=router;
