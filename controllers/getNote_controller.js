53bdb3f29741cd7949f7bfd85b9bc8f030817ddf
Deepak Singh <59962384+deepak-singh343@users.noreply.github.com>
2020-04-02 17:28:55 +0530

    Update getNote_controller.js

--- controllers/getNote_controller.js
+++ controllers/getNote_controller.js
@@ -2,11 +2,18 @@ const Note=require('../models/notes');
 const User=require('../models/users');
 module.exports.getNote=async function(req,res)
 {
-    let notes=await Note.find({})
-    .sort('-createdAt')
-    .populate('user')
-    return res.status(200).json({
-        message:"My Notes",
-        notes:notes
-    })
+    try{
+             let users=await User.findById(req.params.id).select('name').populate({path: 'notes', select:'content'})   //{path: 'notes', options: {sort: {createdAt: -1}},select: 'content'}
+             console.log('Users',users);
+            return res.status(200).json({
+                message:"My Notes",
+                users:users
+            });
+    }
+    catch(err)
+    {
+        return res.status(500).json({
+            message:err.message
+        });
+    }
 }
