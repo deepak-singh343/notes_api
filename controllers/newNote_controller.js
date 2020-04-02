028e362113d066220a9ce8d10caa14adfb96a895
Deepak Singh <59962384+deepak-singh343@users.noreply.github.com>
2020-04-02 17:29:36 +0530

    Update newNote_controller.js

--- controllers/newNote_controller.js
+++ controllers/newNote_controller.js
@@ -1,16 +1,25 @@
 const Note=require('../models/notes');
-module.exports.create=async function(req,res)
+const User=require('../models/users');
+module.exports.createNote=async function(req,res)
 {
-    const note=new Note({
-        content:req.body.content,
-        user:req.body.user
-    });
-    try{
-        const newNote=await note.save();
-        res.status(201).json(newNote);
-    }catch(err){
-        res.status(400).json({
-            message:err.message
+    try {
+        // const user = await User.findOne({_id:req.body.user});
+        const user = await User.findOne({_id:req.user});
+        if(!user){
+            throw new Error("User is Not Found.");
+        }
+        const newNote = await Note.create({
+            content:req.body.content,
+            user: req.body.user,
+            // user: req.user
         })
+        user.notes.push(newNote);
+        user.save();
+        console.log(user);
+        return res.json({success: true, message: "note added Successfully!!"});
+
+    } catch (e) {
+        console.error("Error while adding new note.");
+        return res.json({success: false, message: "Error while adding note." + e});
     }
 }    
