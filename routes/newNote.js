065f2b926134eabb6d81fd16eea9134dccccc3ad
Deepak Singh <59962384+deepak-singh343@users.noreply.github.com>
2020-04-02 20:24:33 +0530

    Update newNote.js

--- routes/newNote.js
+++ routes/newNote.js
@@ -1,5 +1,6 @@
 const express=require('express');
 const router=express.Router();
+const passport=require('passport');
 const newNote_controller=require('../controllers/newNote_controller');
-router.post('/',newNote_controller.create);
+router.post('/',passport.authenticate('jwt',{session:false}),newNote_controller.create);
 module.exports=router;
